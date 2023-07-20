329cf67792fe08d3f11b8b1f749d20dc3ed7b366
Tar0k <73.tarok@gmail.com>
2023-07-20 23:05:58 +0400

    Initial commit

---
+++ Task 1.py
@@ -0,0 +1,17 @@
+# задача 1 сложная необязательная Посчитать сумму цифр любого целого или вещественного числа.
+# Через строку решать нельзя.
+from decimal import Decimal
+
+number = Decimal(input("Введите число: "))
+print(number)
+while number % 1 > 0:
+    number *= 10
+number = int(number)
+
+nums = []
+while number > 10:
+    nums.append(number % 10)
+    number //= 10
+nums.append(number)
+print(nums)
+print(f"Сумма цифр: {sum(nums)}")
---
+++ Task 2.py
@@ -0,0 +1,44 @@
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+# теперь надо проверить ее практически
+# в цикле 100 раз прогоняем
+# каждый раз генерируем случайное количество предикат от 3 до 15
+# и конечно со случайным булевым значением
+# и засекаем общее время выполнения программы
+# юзаем библиотеки random и time
+# предикаты НЕ ЗАДАЕМ как целое число!
+from array import array
+import random
+from functools import reduce
+from datetime import datetime
+
+
+def check_morgan():
+    predicate_number = random.randint(3, 15)
+    predicates = array('b', [random.choice([True, False]) for i in range(predicate_number)])
+    print(f"Рандомный предикат: {''.join([str(item) for item in predicates])}", end=" ")
+
+    left_side = not reduce(left_function, predicates)
+    right_side = reduce(right_function, predicates[1:], not predicates[0])
+    if left_side == right_side:
+        print("Утверждение истинно")
+    else:
+        print("Что-то пошло не так")
+
+
+def left_function(accumulator, next_element):
+    return accumulator or next_element
+
+
+def right_function(accumulator, next_element):
+    return accumulator and not next_element
+
+
+start_time = datetime.now()
+
+for iterator in range(100):
+    check_morgan()
+
+print(f"Время выполнения {datetime.now() - start_time}")
+
+
+
